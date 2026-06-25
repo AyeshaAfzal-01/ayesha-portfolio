5fe3011351cafc550d341c6ed04e52ee376f27d4
Ayesha Afzal <ayesha.taisee1@gmail.com>
2026-06-25 14:23:35 +0500

    responsive navbar

---
+++ src/assets/assets.js
@@ -0,0 +1,13 @@
+import github_icon from './github.png'
+import hamburger_icon from './hamburger.png'
+import gmail_icon from './gmail.png'
+import linkedin_icon from './linkedin.png'
+
+const assets = {
+    github_icon,
+    hamburger_icon,
+    gmail_icon,
+    linkedin_icon,
+}
+
+export default assets
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ThemeToggle from "./ThemeToggle";
+import assets from "../assets/assets";
 
 const Navbar = () => {
+  const [showHamburgerItems, setShowHamburgerItems] = useState(false)
+
   return (
     <nav>
-      <div className='flex justify-between items-center px-12 py-2'>
-        <div className='text-pink-500 font-bold font-display text-2xl cursor-pointer hover:text-pink-400'>ayesha.</div>
-        <div className='flex gap-4 items-center'>
-        <ul className='font-body text-gray-400 text-sm tracking-wider font-semibold cursor-pointer flex gap-6'>
-          <li>WORK</li>
-          <li>ABOUT</li>
-          <li>CONTACT</li>
-        </ul>
-        <ThemeToggle />
+      <div className='flex justify-between items-center px-6 md:px-12 py-2 w-screen'>
+        
+        <div className='text-pink-500 font-bold font-display text-2xl cursor-pointer hover:text-pink-400'>
+          ayesha.
         </div>
+
+        <div className='flex items-center gap-4'>
+          
+          {/* Desktop Menu */}
+          <ul className='hidden md:flex font-body text-gray-400 text-sm tracking-wider font-semibold gap-6'>
+            <li>WORK</li>
+            <li>ABOUT</li>
+            <li>CONTACT</li>
+          </ul>
+
+          <ThemeToggle />
+
+          {/* Mobile Hamburger */}
+          <img onClick={() => setShowHamburgerItems(!showHamburgerItems)}
+            src={assets.hamburger_icon}
+            alt="Menu"
+            className='w-6 h-6 cursor-pointer md:hidden'
+          />
         </div>
-        <hr className='text-gray-300' />
+
+      </div>
+
+    {showHamburgerItems && (
+  <div className="md:hidden px-6 py-6 h-screen  mt-12">
+    <ul className="flex flex-col justify-center items-center gap-4 text-gray-400 font-semibold">
+      <li>WORK</li>
+      <li>ABOUT</li>
+      <li>CONTACT</li>
+    </ul>
+  </div>
+)}
+      <hr className='text-gray-300' />
     </nav>
   )
 }
