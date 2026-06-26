f9c0b558ba23dc3025f9145d3c5bb7efbcd32276
Ayesha Afzal <ayesha.taisee1@gmail.com>
2026-06-26 17:32:37 +0500

    about section changes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
         <div className=""><img src={assets.about_img} className='w-800 h-100' alt="about-img" /></div>
         <div>
           <h2 className='text-6xl text-dark font-semibold mb-3 font-display'>About me</h2>
-          <p className='text-gray-500 font-body text-justify'>believe great software sits at the intersection of functionality and experience. As a Full-Stack Developer, I enjoy crafting fast, intuitive, and visually appealing applications that solve real-world problems. Whether I'm building a responsive frontend, designing APIs, or exploring AI technologies, I'm always looking for ways to learn, improve, and create work that people genuinely enjoy using. Every project is an opportunity to turn ideas into experiences that feel seamless and purposeful.</p>
+          <p className='text-gray-500 font-body text-justify'>I believe great software sits at the intersection of functionality and experience. As a Full-Stack Developer, I enjoy crafting fast, intuitive, and visually appealing applications that solve real-world problems. Whether I'm building a responsive frontend, designing APIs, or exploring AI technologies, I'm always looking for ways to learn, improve, and create work that people genuinely enjoy using. Every project is an opportunity to turn ideas into experiences that feel seamless and purposeful.</p>
           <Skills/>
         </div>
       </div>
