46188e66a675fdd25aab9e8e5c351344d3c0a4d4
Ayesha Afzal <ayesha.taisee1@gmail.com>
2026-06-29 13:04:56 +0500

    scroll fix in skill.jsx final

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -114,17 +114,17 @@ const Skills = () => {
       <div className="mt-8 overflow-x-auto scrollbar-hide">
         <div className="flex w-max gap-4">
           <TabButton
-            selectTab={() => handleTabChange("skills")}
-            active={tab === "skills"}
-          >
-            Skills
-          </TabButton>
-
-          <TabButton
             selectTab={() => handleTabChange("education")}
             active={tab === "education"}
           >
             Education
+          </TabButton>
+
+            <TabButton
+            selectTab={() => handleTabChange("skills")}
+            active={tab === "skills"}
+          >
+            Skills
           </TabButton>
 
           <TabButton
